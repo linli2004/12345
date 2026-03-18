4c746d1133a9c4ec80ed97c9418a68190f3d0c60
sty <2482757355@qq.com>
2026-03-18 11:14:43 +0800

    feature: 基层不允许退回校验

--- lamp-base/lamp-base-biz/src/main/java/top/tangyh/lamp/base/service/impl/NormalWorkOrderTaskServiceImpl.java
+++ lamp-base/lamp-base-biz/src/main/java/top/tangyh/lamp/base/service/impl/NormalWorkOrderTaskServiceImpl.java
@@ -31,6 +31,7 @@ import top.tangyh.lamp.model.entity.system.SysUser;
 import top.tangyh.lamp.msg.biz.MsgBiz;
 import top.tangyh.lamp.msg.vo.update.ExtendMsgPublishVO;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -216,6 +217,9 @@ public class NormalWorkOrderTaskServiceImpl extends SuperServiceImpl<NormalWorkO
     @Override
     @Transactional(rollbackFor = Exception.class)
     public boolean basicBackNormalWorkOrder(NormalWorkOrderTaskActionVO backVO) {
+        //判断是否可以退回
+        NormalWorkOrder workOrderTemp = normalWorkOrderManager.getOne(Wraps.<NormalWorkOrder>lbQ().eq(NormalWorkOrder::getOrderNo, backVO.getOrderNo()));
+        ArgumentAssert.isFalse(Constant.ALLOW_BACK_0.equals(workOrderTemp.getAllowBack()) || (Constant.ALLOW_BACK_2.equals(workOrderTemp.getAllowBack()) && LocalDateTime.now().isAfter(workOrderTemp.getAllowBackTime())), "此工单已不允许退回");
         //新增 基层退回的办理动态
         WorkOrderDynamic dynamicTemp = BeanUtil.copyProperties(backVO, WorkOrderDynamic.class, "id");
         dynamicTemp.setOrderNo(backVO.getOrderNo());
